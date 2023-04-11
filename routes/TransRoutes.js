6662bde160c21963c0d7400d91f25f9d40560dae
mohit khairnar <mvk1407@gmail.com>
2023-04-11 20:07:45 +0530

    refactor: final update

--- routes/TransRoutes.js
+++ routes/TransRoutes.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 
 const TransactionSchema = require('../models/Transaction')
 
-//test
+
 router.get('/transactions/:address', async(req, res)=> {
     try {
         const address = req.params.address;
